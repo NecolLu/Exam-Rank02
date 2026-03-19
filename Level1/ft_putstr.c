966795da9f82ad34d371dcfd1a111df0dfab502c
Zhi Sin Loo <zloo@u81z08s03.42kl.edu.my>
2026-03-19 14:04:10 +0800

    updates

--- Level1/ft_putstr.c
+++ Level1/ft_putstr.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zloo <zloo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/17 17:42:20 by zloo              #+#    #+#             */
-/*   Updated: 2026/03/19 13:51:15 by zloo             ###   ########.fr       */
+/*   Created: 2026/03/19 13:52:57 by zloo              #+#    #+#             */
+/*   Updated: 2026/03/19 13:56:12 by zloo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	first_word(char *str)
+void	ft_putstr(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	while (str[i] != ' '&&  str[i] != '\0' && str[i] != '\t')
+	while (str[i] != '\0')
 	{
 		write(1, &str[i], 1);
 		i++;
@@ -29,8 +27,8 @@ void	first_word(char *str)
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
-		first_word(argv[1]);
-	write(1, "\n", 1);
+		ft_putstr(argv[1]);
+	else
+		write(1, "\n", 1);
 	return (0);
 }
-	
